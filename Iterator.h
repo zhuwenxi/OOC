341938a53287e165f016835c0881864f208c4063
zhuwenxi <zhuwenxi8935@gmail.com>
2015-12-08 22:16:15 +0800

    Start to implement the Iterator.

---
+++ Iterator.h
@@ -0,0 +1,3 @@
+#ifndef __ITERATOR_H__
+#define __ITERATOR_H__
+#endif
---
+++ Iterator_r.h
@@ -0,0 +1,25 @@
+#ifndef __ITERATOR_R_H__
+#define __ITERATOR_R_H__
+
+
+
+
+#include "Object_r.h"
+
+struct Iterator
+{
+	struct Object _;
+	struct Object * data;
+	int index;
+};
+
+struct IteratorClass
+{
+	struct Class _;
+	struct Object * (*start)(const struct Iterator * iter);
+	struct Object * (*end)(const struct Iterator * iter);
+	struct Object * (*next)(const struct Iterator * iter);
+};
+
+
+#endif
